81ea28acd903be262396609b88208b25b563df51
herilion <heritierlionge@yahoo.com>
2022-09-14 15:16:12 +0200

    add .env

--- src/Composants/Identifiants.jsx
+++ src/Composants/Identifiants.jsx
@@ -1,7 +1,7 @@
 const Identifiants = () => {
   return {
-    clientId: "af27c3b99f1944b285e63baa7b33c5f3",
-    clientSecret: "dd6f1c4b77b8498193d77dd0aa26b511",
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    clientSecret: process.env.REACT_APP_CLIENT_SECRET_ID,
   };
 };
 
